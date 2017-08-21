ab619a7ff618bacada5d3d274d3801b662f812bd
Troy Patrick <patrictroy@gmail.com>
2017-08-21 20:50:02 +1000

    Change max_vel to 1.
    Drive slow to help improve fidelity of map

--- code/drive_rover.py
+++ code/drive_rover.py
@@ -61,11 +61,11 @@ class RoverState():
         # get creative in adding new fields or modifying these!
         self.stop_forward = 50 # Threshold to initiate stopping
         self.go_forward = 500 # Threshold to go forward again
-        self.max_vel = 2 # Maximum velocity (meters/second)
+        self.max_vel = 1 # Maximum velocity (meters/second)
         # Image output from perception step
         # Update this image to display your intermediate analysis steps
         # on screen in autonomous mode
-        self.vision_image = np.zeros((160, 320, 3), dtype=np.float) 
+        self.vision_image = np.zeros((160, 320, 3), dtype=np.float)
         # Worldmap
         # Update this image with the positions of navigable terrain
         # obstacles and rock samples
