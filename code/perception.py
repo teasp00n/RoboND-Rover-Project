586487ecc3e025b03ed5ab21f312ee39bd2b2ecf
Troy Patrick <patrictroy@gmail.com>
2017-08-23 22:06:44 +1000

    Formatting and documentation of private functions

--- code/perception.py
+++ code/perception.py
@@ -2,9 +2,18 @@ import numpy as np
 import cv2
 
 
+MAX_ROLL = 2
+MAX_PITCH = 2
+
+
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
-def color_thresh(img, rgb_thresh=(160, 160, 160)):
+def _color_thresh(img, rgb_thresh=(160, 160, 160)):
+    """
+    Returns an array of 1s and 0s where a 1 indicates the source pixel was
+    above the given threshold and 0 indicates it was not. The returned array
+    will be of the same shape as the source array, but is a single channel.
+    """
     # Create an array of zeros same xy size as img, but single channel
     color_select = np.zeros_like(img[:, :, 0])
 
@@ -23,18 +32,27 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
 
 # TODO: investigate other methods for this
 # TODO: come up with our own selector based on images
-def find_rocks(img, selector=(110, 110, 50)):
+def _find_rocks(img, selector=(110, 110, 50)):
+    """
+    Returns an array of 1s and 0s where a 1 indicates a pixel matched the given
+    selector, and a 0 indicates it has not.
+    This is a variation of _color_thresh specifically for finding rocks (the
+    selector is a little different).
+    """
     gold_pix = ((img[:, :, 0] > selector[0])
                & (img[:, :, 1] > selector[1])
                & (img[:, :, 2] < selector[2]))
 
-    color_select = np.zeros_like(img[:, :, 0])
-    color_select[gold_pix] = 1
-    return color_select
+    rock_map = np.zeros_like(img[:, :, 0])
+    rock_map[gold_pix] = 1
+    return rock_map
 
 
 # Define a function to convert from image coords to rover coords
-def object_coords(binary_img):
+def _rover_coords(binary_img):
+    """
+    Returns coordinates with the rover camera as the origin (0, 0)
+    """
     # Identify nonzero pixels
     ypos, xpos = binary_img.nonzero()
     # Calculate pixel positions with reference to the rover position being at
@@ -45,7 +63,10 @@ def object_coords(binary_img):
 
 
 # Define a function to convert to radial coords in rover space
-def to_polar_coords(x_pixel, y_pixel):
+def _to_polar_coords(x_pixel, y_pixel):
+    """
+    Converts cartesian coordinates to polar coordinates
+    """
     # Convert (x_pixel, y_pixel) to (distance, angle)
     # in polar coordinates in rover space
     # Calculate distance to each pixel
@@ -56,31 +77,47 @@ def to_polar_coords(x_pixel, y_pixel):
 
 
 # Define a function to map rover space pixels to world space
-def rotate_pix(xpix, ypix, yaw):
+def _rotate_pix(xpix, ypix, yaw):
+    """
+    Applies a rotation matrix to the pixels, xpix and ypix, given the angle as
+    yaw.
+    """
     # Convert yaw to radians
     yaw_rad = yaw * np.pi / 180
     xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
-
     ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
     # Return the result
     return xpix_rotated, ypix_rotated
 
 
-def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale):
+def _translate_pix(x, y, x_offset, y_offset, scale):
+    """
+    Applies a simple translation matrix to x and y given the offsets for each
+    and the scale.
+    """
     # Apply a scaling and a translation
-    xpix_translated = (xpix_rot / scale) + xpos
-    ypix_translated = (ypix_rot / scale) + ypos
+    xpix_translated = (x / scale) + x_offset
+    ypix_translated = (y / scale) + y_offset
     # Return the result
     return xpix_translated, ypix_translated
 
 
 # Define a function to apply rotation and translation (and clipping)
 # Once you define the two functions above this function should work
-def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+def _pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+    """
+    Converts from rover coordinate space to world coordinate space by:
+    1. Apply a rotation matrix on the coordinates to correct for the rovers
+    current yaw.
+    2. Apply a translation matrix on the resultant coordinates to adjust for
+    the rovers current position in the world.
+    3. Clip the resulting pixels to ensure they fall within the bounds of our
+    world coordinate space.
+    """
     # Apply rotation
-    xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
+    xpix_rot, ypix_rot = _rotate_pix(xpix, ypix, yaw)
     # Apply translation
-    xpix_tran, ypix_tran = translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
+    xpix_tran, ypix_tran = _translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
     # Perform rotation, translation and clipping all at once
     x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size - 1)
     y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size - 1)
@@ -88,7 +125,11 @@ def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
     return x_pix_world, y_pix_world
 
 
-def field_of_view(img, src, dst):
+def _field_of_view(img, src, dst):
+    """
+    Provides a mask to index our array such that we don't map values out of
+    view of the cameras field of view
+    """
     M = cv2.getPerspectiveTransform(src, dst)
     fov = cv2.warpPerspective(np.ones_like(img[:, :, 0]), M, (img.shape[1],
                               img.shape[0]))
@@ -96,7 +137,11 @@ def field_of_view(img, src, dst):
 
 
 # Define a function to perform a perspective transform
-def perspect_transform(img, src, dst):
+def _perspect_transform(img, src, dst):
+    """
+    Applies a perspective transform of the image given the source and
+    destination pixel positions.
+    """
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, (img.shape[1],
                                  img.shape[0]))  # keep same size as input image
@@ -126,6 +171,10 @@ def perception_step(Rover):
     # Rover.nav_dists = rover_centric_pixel_distances
     # Rover.nav_angles = rover_centric_angles
 
+    OBSTACLE_CHANNEL = 0
+    ROCK_CHANNEL = 1
+    NAVIGABLE_CHANNEL = 2
+
     scale = 10
     bottom_offset = 6
 
@@ -137,39 +186,42 @@ def perception_step(Rover):
                               [Rover.img.shape[1] / 2 - (scale / 2), Rover.img.shape[0] - 2 * (scale / 2) - bottom_offset],
                               ])
 
-    fov = field_of_view(Rover.img, source, destination)
-    warped = perspect_transform(Rover.img, source, destination)
+    fov = _field_of_view(Rover.img, source, destination)
+    warped = _perspect_transform(Rover.img, source, destination)
 
     navigable_threshold = (160, 160, 160)
-    navigable_terrain = color_thresh(warped, navigable_threshold)
-    Rover.vision_image[:, :, 2] = navigable_terrain * 255
+    navigable_terrain = _color_thresh(warped, navigable_threshold)
+    Rover.vision_image[:, :, NAVIGABLE_CHANNEL] = navigable_terrain * 255
 
     obstacles = np.absolute(np.float32(navigable_terrain) - 1) * fov
-    Rover.vision_image[:, :, 0] = obstacles * 255
+    Rover.vision_image[:, :, OBSTACLE_CHANNEL] = obstacles * 255
 
     worldmap_size = Rover.worldmap.shape[0]
 
-    rover_x, rover_y = object_coords(navigable_terrain)
-    rover_x_world, rover_y_world = pix_to_world(rover_x, rover_y,
+    rover_x, rover_y = _rover_coords(navigable_terrain)
+    rover_x_world, rover_y_world = _pix_to_world(rover_x, rover_y,
                                                 Rover.pos[0], Rover.pos[1], Rover.yaw, worldmap_size, scale)
 
-    obstacles_x, obstacles_y = object_coords(obstacles)
-    obstacles_x_world, obstacles_y_world = pix_to_world(obstacles_x, obstacles_y,
+    obstacles_x, obstacles_y = _rover_coords(obstacles)
+    obstacles_x_world, obstacles_y_world = _pix_to_world(obstacles_x, obstacles_y,
                                                         Rover.pos[0], Rover.pos[1], Rover.yaw, worldmap_size, scale)
 
-    rock_map = find_rocks(warped)
+    rock_map = _find_rocks(warped)
     if rock_map.any():
-        rock_x, rock_y = object_coords(rock_map)
-        rock_x_world, rock_y_world = pix_to_world(rock_x, rock_y,
+        rock_x, rock_y = _rover_coords(rock_map)
+        rock_x_world, rock_y_world = _pix_to_world(rock_x, rock_y,
                                                   Rover.pos[0], Rover.pos[1], Rover.yaw, worldmap_size, scale)
 
-        rock_dist, rock_angles = to_polar_coords(rock_x, rock_y)
+        rock_dist, rock_angles = _to_polar_coords(rock_x, rock_y)
         rock_anchor_index = np.argmin(rock_dist)
         rock_anchor_x = rock_x_world[rock_anchor_index]
         rock_anchor_y = rock_y_world[rock_anchor_index]
 
+        # NOTE: usually when mapping we would check if our plane is correct but
+        # for small items like rocks it doesn't matter as much
         Rover.worldmap[rock_anchor_x, rock_anchor_y, 1] += 1
-        Rover.vision_image[:, :, 1] = rock_map * 255
+
+        Rover.vision_image[:, :, ROCK_CHANNEL] = rock_map * 255
         Rover.samples_located += 1
 
         # if we found a rock lets go towards it
@@ -178,16 +230,30 @@ def perception_step(Rover):
         Rover.nav_angles = rock_angles
 
     else:
-        Rover.vision_image[:, :, 1] = 0
+        Rover.vision_image[:, :, ROCK_CHANNEL] = rock_map * 0
         # no rock so we abide by our general navigation principles
         Rover.see_sample = 0
-        dist, angles = to_polar_coords(rover_x, rover_y)
+        dist, angles = _to_polar_coords(rover_x, rover_y)
         Rover.nav_dists = dist
         Rover.nav_angles = angles
 
     # we are only mapping in one plane. we need to make sure we don't map anything when the robot is all topsy-turvy
-    if Rover.roll < 3 and Rover.pitch < 3:
+    if _should_map(Rover.roll, Rover.pitch):
         Rover.worldmap[rover_y_world, rover_x_world, 2] += 1
-        Rover.worldmap[obstacles_y_world, obstacles_x_world, 0] += 1
+        # if this part of the map has been previously deemed navigable then it probably is - we just can't see through rocks
+        Rover.worldmap[obstacles_y_world, obstacles_x_world, OBSTACLE_CHANNEL] += 1
+        # if Rover.worldmap[obstacles_y_world, obstacles_x_world, NAVIGABLE_CHANNEL] == 0:
+            # Rover.worldmap[obstacles_y_world, obstacles_x_world, OBSTACLE_CHANNEL] += 1
 
     return Rover
+
+
+def _should_map(roll, pitch):
+    return roll < _from_origin(MAX_ROLL) and pitch < _from_origin(MAX_PITCH)
+
+
+def _from_origin(degrees):
+    if degrees < 180:
+        return degrees
+    else:
+        return 360 - degrees
